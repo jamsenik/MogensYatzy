3e278a0e7f8371f45e06a43ad9b1c1e354a3e95a
Hans Kyndesgaard <hans@Hanss-MacBook-Pro.local>
2012-07-20 19:56:47 +0200

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Hans Kyndesgaard on 20/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by Hans Kyndesgaard on 20/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloWorldTests : SenTestCase
+
+@end
